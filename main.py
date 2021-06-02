4dec80c9d8ad3f9d88fd5e1476e4b4c544933f6e
Osv04 <oscar.veloz4@gmail.com>
2021-06-01 22:54:49 -0400

    Estadarizacion de variables y agregar empresas, cambio de path para las empresas

--- main.py
+++ main.py
@@ -2,17 +2,42 @@ from __future__ import print_function
 from mailmerge import MailMerge
 from datetime import date
 
-institucion1 = input("Digite Nombre de la Institucion")
+print("*****************************************************")
+print("Propiedad de Oscar Veloz Arencibia")
+print("*****************************************************")
+print(" Empresas")
+print(""" 1) Automaki SRL
+ 2) Ducto Limpio S.D. SRL
+ 3) Servicios Diversos Arnaud SRL
+ 4) Red Dot Tech SAS
+ 5) Latin American Service SRL """)
+print("*****************************************************")
+
+empresas = ["Automaki SRL","Ducto Limpio S.D. SRL","Servicios Diversos Arnaud SRL","Red Dot Tech SAS","Latin American Service SRL"]
+eleccion = input("Digite que tipo de empresa va trabajar ")
+eleccion = int(eleccion) - 1 
+#Ayuda para poder poner nombre de los docs
+selectedcompany = empresas[eleccion]
+
+
+#Declaracion de variables estandar en proceso de licitacion
+institucion1 = input(" Digite Nombre de la Institucion")
 institucion1 = str(institucion1)
 today = date.today()
 day = today.strftime("%d/%m/%Y")
 day = str(day)
-ID1 = input("Digite ID del Proceso")
+ID1 = input(" Digite ID del Proceso")
 ID1 = str(ID1)
+objeto = input(" El objeto del proceso")
+objeto = str(objeto)
 
-def SNCCF042_InformacionOferenteSDA(institucion1,day,ID1):
 
-    template = "SNCC_F042_Formulario Oferente SDA.docx"
+
+
+
+def SNCCF042_InformacionOferenteSDA(selectedcompany, template,institucion1,day,ID1):
+
+    template = template
     document = MailMerge(template)
 
     document.merge(
@@ -20,12 +45,21 @@ def SNCCF042_InformacionOferenteSDA(institucion1,day,ID1):
         Date = day,
         Id = ID1
     )
-    document.write('test-output.docx')
+    document.write(ID1+'SNCCF034_PresentacionDeOferta' + selectedcompany +'.docx')
 
 
-def SNCCF034_PresentacionDeOferta_SDA():
-    template = "SNCC_F042_Formulario Oferente SDA.docx"
+def SNCCF034_PresentacionDeOferta(selectedcompany, template,institucion1,day,ID1,objeto):
+    
+    template = template
     document = MailMerge(template)
+    document.merge(
+        Institucion = institucion1,
+        Date = day,
+        Id = ID1,
+        Objeto = objeto
+    )
+    document.write(ID1 +'SNCCF042_InformacionOferente' + selectedcompany +'.docx')
 
-    
-    
+def OfertaSDA(template,institucion1,day,ID1,objeto):
+    SNCCF034_PresentacionDeOferta(template,institucion1,day,ID1,objeto)
+
